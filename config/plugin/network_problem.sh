390c3e82c85be4d2464bba87709af706d90b3d01
Leonardo Dagnino <leodag.sch@gmail.com>
2023-07-21 17:00:49 +0200

    feat: support bind-mounting netfilter directory

--- config/plugin/network_problem.sh
+++ config/plugin/network_problem.sh
@@ -7,24 +7,29 @@ readonly OK=0
 readonly NONOK=1
 readonly UNKNOWN=2
 
-# "nf_conntrack" replaces "ip_conntrack" - support both
-readonly NF_CT_COUNT_PATH='/proc/sys/net/netfilter/nf_conntrack_count'
-readonly NF_CT_MAX_PATH='/proc/sys/net/netfilter/nf_conntrack_max'
-readonly IP_CT_COUNT_PATH='/proc/sys/net/ipv4/netfilter/ip_conntrack_count'
-readonly IP_CT_MAX_PATH='/proc/sys/net/ipv4/netfilter/ip_conntrack_max'
+# support custom values for conntrack path in case of a bind mount
+if [[ -z $CT_COUNT_PATH || -z $CT_MAX_MATH ]]; then
+    # "nf_conntrack" replaces "ip_conntrack" - support both
+    readonly NF_CT_COUNT_PATH='/proc/sys/net/netfilter/nf_conntrack_count'
+    readonly NF_CT_MAX_PATH='/proc/sys/net/netfilter/nf_conntrack_max'
+    readonly IP_CT_COUNT_PATH='/proc/sys/net/ipv4/netfilter/ip_conntrack_count'
+    readonly IP_CT_MAX_PATH='/proc/sys/net/ipv4/netfilter/ip_conntrack_max'
 
-if [[ -f $NF_CT_COUNT_PATH ]] && [[ -f $NF_CT_MAX_PATH ]]; then
-  readonly CT_COUNT_PATH=$NF_CT_COUNT_PATH
-  readonly CT_MAX_PATH=$NF_CT_MAX_PATH
-elif [[ -f $IP_CT_COUNT_PATH ]] && [[ -f $IP_CT_MAX_PATH ]]; then
-  readonly CT_COUNT_PATH=$IP_CT_COUNT_PATH
-  readonly CT_MAX_PATH=$IP_CT_MAX_PATH
-else
-  exit $UNKNOWN
+    if [[ -f $NF_CT_COUNT_PATH ]] && [[ -f $NF_CT_MAX_PATH ]]; then
+        readonly CT_COUNT_PATH=$NF_CT_COUNT_PATH
+        readonly CT_MAX_PATH=$NF_CT_MAX_PATH
+    elif [[ -f $IP_CT_COUNT_PATH ]] && [[ -f $IP_CT_MAX_PATH ]]; then
+        readonly CT_COUNT_PATH=$IP_CT_COUNT_PATH
+        readonly CT_MAX_PATH=$IP_CT_MAX_PATH
+    else
+        exit $UNKNOWN
+    fi
 fi
 
-readonly conntrack_count=$(< $CT_COUNT_PATH) || exit $UNKNOWN
-readonly conntrack_max=$(< $CT_MAX_PATH) || exit $UNKNOWN
+readonly conntrack_count
+conntrack_count=$(< "$CT_COUNT_PATH") || exit $UNKNOWN
+readonly conntrack_max
+conntrack_max=$(< "$CT_MAX_PATH") || exit $UNKNOWN
 readonly conntrack_usage_msg="${conntrack_count} out of ${conntrack_max}"
 
 if (( conntrack_count > conntrack_max * 9 /10 )); then
